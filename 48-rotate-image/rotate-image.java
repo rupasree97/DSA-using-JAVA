aad8a60d0d57cc0c077c20091b1d369dc9b46f33
Rupa sree <rupabodem.97@gmail.com>
2026-01-11 19:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (6.96%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int l = 0, r = matrix.length-1;
+        while(l<r)
+        {
+        for(int i = 0; i<r-l;i++)
+        {
+            int top =l,bottom = r;
+            int topleft = matrix[top][l+i];
+            matrix[top][l+i] = matrix[bottom-i][l];
+            matrix[bottom-i][l] = matrix[bottom][r-i];
+            matrix[bottom][r-i] = matrix[top+i][r];
+            matrix[top+i][r] = topleft;
+        }
+        l++;
+        r--;
+        }
+        
+    }
+}
