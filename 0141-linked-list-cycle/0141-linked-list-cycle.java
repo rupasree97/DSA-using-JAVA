b6b040d77bca77cbf7b547cc6feaccbbbf7b0136
Rupa sree <rupabodem.97@gmail.com>
2026-02-18 21:07:34 +0530

    Time: 0 ms (100%), Space: 46.9 MB (52.27%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,14 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+         // hashmap -- s.c-0(n), t.c - 0(n)
+          /*floyd's algorithm (fast [2 steps] and slow pointers[1 step]) -- optimal  
+          => s.c-0(1) , T.c - (n) */
+          /*If cycle is detected then there is situation where two pointers will be  at a same position */
+
         ListNode fast = head;
         ListNode slow = head;
         while(fast!=null && fast.next!=null)
         {
              slow = slow.next;
              fast = fast.next.next;
-             if(slow==fast) return true;
-
+             if(slow==fast) return true; 
         }
       return false;  
     }
