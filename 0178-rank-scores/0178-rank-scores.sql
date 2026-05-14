ca10921278abb2505857e25021b70bb421367c83
Rupa sree <rupabodem.97@gmail.com>
2026-05-14 20:16:06 +0530

    Time: 1681 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select s1.score, (select count(distinct s2.score) from scores s2 where s2.score >= s1.score) as `rank` from scores s1 order by s1.score desc;
 -- select score, dense_rank() over (order by score desc) as 'rank' from scores order by score desc;
 # t.c - o(n log n) , .sc - o(n)
+select s1.score, (select count(distinct s2.score) from scores s2 where s2.score >= s1.score) as `rank` from scores s1 order by s1.score desc;
+#t.c- o(n2) s.c- o(1)
