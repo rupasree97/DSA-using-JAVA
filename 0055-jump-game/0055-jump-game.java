5994490feb17e8ffe12096345540723c399f7f3e
Rupa sree <rupabodem.97@gmail.com>
2026-06-13 21:37:30 +0530

    Time: 1 ms (99.97%), Space: 47.9 MB (41.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
        
-        int goal = nums.length-1;
-        for(int i = nums.length-2; i>=0; i--)
-        {
-            if(i+nums[i]>=goal)
-            {
-                goal = i;
-            }
-        }
-       return goal==0;
+       int jump = nums.length-1;
+       for(int i = nums.length-2; i>=0; i--)
+       {
+    
+        if(nums[i] + i  >= jump )
+        jump = i;
+       }
+       return jump==0;
     }
 }
