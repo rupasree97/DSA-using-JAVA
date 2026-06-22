6c02c7ec9ec94927a7360c2a996cf781719194fa
Rupa sree <rupabodem.97@gmail.com>
2026-06-22 21:44:51 +0530

    Time: 46 ms (6.87%), Space: 43.2 MB (87.41%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        char[] arr = text.toCharArray();
+        int r = 0;
+        while(true)
+        {
+            String word = "balloon";
+            for(char c : word.toCharArray())
+            {
+                boolean found  = false;
+            for(int i = 0; i<arr.length; i++)
+            {
+                if(arr[i] == c){
+                arr[i] = '#';
+                found = true;
+                break;
+                }
+
+              }
+
+        
+
+        if(!found) return r;
+            }
+            r++;
+        }
+       
+    }
+}
