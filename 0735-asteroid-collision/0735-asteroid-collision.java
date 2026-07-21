2283cedb27e929e7c78368b1d26427da1a2e0f32
Rupa sree <rupabodem.97@gmail.com>
2026-07-21 20:07:33 +0530

    Time: 5 ms (69.1%), Space: 47.1 MB (30.51%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,48 +1,37 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        
-            ArrayList <Integer> l = new ArrayList<>();
-            for(int a : asteroids)
-            {
-                l.add(a);
-            }
-        
-        boolean changed = true;
-        while(changed)
-        {
-            changed = false;
-            for(int i = 0; i<l.size()-1; i++)
-            {
-                int left = l.get(i);
-                int right = l.get(i+1);
-                 if(left>0 && right<0)
-                 {
-                      changed = true;
-                    if(Math.abs(left) < Math.abs(right))
-                    {
-                         l.remove(i);
-                    }
-                    else if(Math.abs(left) > Math.abs(right))
-                    {
-                        l.remove(i+1);
-                    }
-                    else
-                    {
-                        l.remove(i+1);
-                        l.remove(i);
-                    }
-                    break;
-              }  
 
-            }
+       Stack<Integer> s = new Stack<Integer>();
+       int n = asteroids.length;
+        for(int i = 0; i<n; i++)
+        {
+               if(asteroids[i] > 0)
+               {
+                s.push(asteroids[i]);
+               }
+               else
+               {
+               while(!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(asteroids[i]))
+               {
+                s.pop();
+               }
+               if(s.isEmpty() || s.peek() < 0)
+               {
+                  s.push(asteroids[i]);
+               }
+               if(s.peek() == Math.abs(asteroids[i]))
+               {
+                s.pop();
+               }
+               }
+               
 
         }
-int res[] = new int[l.size()];
-    for(int i = 0; i< l.size(); i++)
-    {
-        res[i] = l.get(i);
-    }
-
-   return res;
+        int res[] = new int[s.size()];
+        for(int i = s.size()-1; i>=0; i--)
+        {
+            res[i] = s.pop();
+        }
+        return res;
     }
 }
