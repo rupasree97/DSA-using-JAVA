da49637906c02f7c5f4ae28ae7e0d5dd1e218a84
Rupa sree <rupabodem.97@gmail.com>
2026-07-27 14:36:40 +0530

    Time: 1 ms (97.41%), Space: 42.4 MB (90.59%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -8,7 +8,7 @@ class Solution {
      
         while(left<=right)
         {
-             mid =left + (right-left)/2;
+             mid =left + (right-left) /2;
             if(mid * mid == x)
               return mid;
             else if((long)mid * mid >(long) x)
