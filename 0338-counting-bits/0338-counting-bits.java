1ae20ed09d19f9c6d4d66f0e52098031beb6704d
Rupa sree <rupabodem.97@gmail.com>
2026-07-29 22:08:00 +0530

    Time: 2 ms (96.01%), Space: 48.7 MB (51.52%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] countBits(int n) {
         int arr[] = new int[n+1];
-        int i = 0;
-        while(i<=n)
-        {
-             arr[i] = shift(i);
-             i++;
+        
+       for(int i = 0; i<=n;i++)
+       {
+           arr[i] = arr[i>>1] + (i&1);
+        
         }
         return arr;
 
     }
-    public int shift(int i)
-    {
-        int count = 0;
-        while(i>0)
-        {
-            count+=(i&1);
-            i = i>>1;
-        }
-        return count;
-}
+//     public int shift(int i)
+//     {
+//         int count = 0;
+//         while(i>0)
+//         {
+//             count+=(i&1);
+//             i = i>>1;
+//         }
+//         return count;
+// }
 }
