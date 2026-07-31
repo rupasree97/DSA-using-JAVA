daf464f98aefe19b1d2720bfa32e0d60bb2576fe
Rupa sree <rupabodem.97@gmail.com>
2026-07-31 22:50:27 +0530

    Time: 1 ms (63.47%), Space: 43.3 MB (20.9%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for(char c:stones.toCharArray())
+        {
+            if(jewels.contains(String.valueOf(c)))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
