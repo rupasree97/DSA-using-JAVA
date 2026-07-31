9ca3a476e68c0c6920ab92b531d19215f40fc1f4
Rupa sree <rupabodem.97@gmail.com>
2026-07-31 22:35:18 +0530

    Time: 0 ms (100%), Space: 43 MB (70.33%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,18 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int n = s.length();
-        int count = 0;
-        for(int i= n-1;i>=0;i--)
+         int count = 0;
+        for(int i = s.length()-1; i>=0; i--)
         {
-            if(s.charAt(i)== ' '&& count>0)
-            break;
+            if(s.charAt(i) == ' ' && count > 0 )
+            {
+                   break;
+            }
             else if(s.charAt(i)!=' ')
             {
-            count++;
+                    count++;
+                    
             }
         }
-       return count; 
+        return count;
     }
 }
