9843c9adb52a602e1ff977cd074f7e9f4f0821f5
Rupa sree <rupabodem.97@gmail.com>
2026-08-03 21:54:14 +0530

    Time: 4 ms (82.71%), Space: 46.5 MB (25.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
-        return false;
-        char res1[] = s.toCharArray();
-        char res2[] = t.toCharArray();
+        char[] res1 = s.toCharArray();
+        char[] res2 = t.toCharArray();
         Arrays.sort(res1);
         Arrays.sort(res2);
         return Arrays.equals(res1,res2);
-        
-        
     }
 }
