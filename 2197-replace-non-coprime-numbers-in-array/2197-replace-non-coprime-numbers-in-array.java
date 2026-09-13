de19262c3b442959c8cfb932823fb06a30301267
Rupa sree <rupabodem.97@gmail.com>
2026-09-13 13:35:07 +0530

    Time: 25 ms (94.48%), Space: 130.7 MB (77.24%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    private int gcd(int a,int b)
+    {
+        int a1 = a;
+        int b1 = b;
+        while(b!=0)
+        {
+         int temp = a%b;
+         a = b;
+         b = temp;
+        }
+        return a;
+        
+
+        
+    }
+    private int lcm(int a1,int b1)
+    {
+        return (a1/gcd(a1,b1))*b1;
+    }
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        ArrayList<Integer> res = new ArrayList<>();
+       for(int x:nums)
+       {
+         while(!res.isEmpty())
+         {
+            int last = res.get(res.size()-1);
+            if(gcd(x,last) == 1)
+            break;
+            x = lcm(x,last);
+            res.remove(res.size()-1);
+
+         }
+         res.add(x);
+
+       }
+        
+      return res;
+    }
+}
